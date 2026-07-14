e73d2af32b0b822049761227b2d816a2afd87315
Furkan-ctrl <furkannagiyev20071@gmail.com>
2026-07-14 15:01:43 +0400

    feat : Add RefreshToken expiration to .yaml file - Add new RefreshToken class - Add UserRepository - Add new SQL Table for RefreshTokens

---
+++ user-service/src/main/java/com/smartbank/userservice/model/RefreshToken.java
@@ -0,0 +1,38 @@
+package com.smartbank.userservice.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "expiry_date", nullable = false)
+    private Instant expiryDate;
+
+    @Column(nullable = false)
+    private boolean revoked;
+
+    public boolean isExpired() {
+        return Instant.now().isAfter(this.expiryDate);
+    }
+}
